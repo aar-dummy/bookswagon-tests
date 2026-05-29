86ef320b5dca9d31d7c58d96f56cd9cd829bc344
Aarthy Reddy <aarthyr@amazon.com>
2026-05-29 10:49:11 +0530

    Fix Chrome headless options for Jenkins on Windows

--- conftest.py
+++ conftest.py
@@ -18,10 +18,15 @@ def get_driver():
     else:
         opts = ChromeOptions()
         if HEADLESS:
-            opts.add_argument("--headless")
+            opts.add_argument("--headless=new")
         opts.add_argument("--no-sandbox")
         opts.add_argument("--disable-dev-shm-usage")
+        opts.add_argument("--disable-gpu")
         opts.add_argument("--window-size=1920,1080")
+        opts.add_argument("--disable-extensions")
+        opts.add_argument("--proxy-server=direct://")
+        opts.add_argument("--proxy-bypass-list=*")
+        opts.add_argument("--ignore-certificate-errors")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=opts)
 
     driver.implicitly_wait(IMPLICIT_WAIT)
